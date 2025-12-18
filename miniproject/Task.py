c4975ccae15ff6ee0affd2f4cf06551e679e4703
TsionMengistu12 <149229052+TsionMengistu12@users.noreply.github.com>
2025-12-18 21:44:51 +0300

    mini project: file and work set up

--- miniproject/Task.py
+++ miniproject/Task.py
@@ -1,23 +1,27 @@
 class Task:
     # 
-    def __init__(self, task):
-        self.task = []
+    def __init__(self, tasks, task_id, title, completed=False):
+        self.tasks = []
+        self.id = task_id
+        self.title = title
+        self.completed = completed
 
-    #Adding tasks
+    # Adds tasks
     def add_to_do(self, id, name, status):
         self.task.append(id)
         self.task.append(name)
         self.task.append(status)
 
-    # returns all tasks
+    # Returns all tasks
     def view_to_do(self):
         return self.task
     
-    # changes the title and completion status
+    # Changes the title and completion status
     def update_to_do(self,change_id, new_name, new_status):
         self.task[change_id] = str(new_name), new_status
 
-    # delete task
+    # Delete task
     def delete_to_do(self, delete_id):
-        self.task[delete_id]
+        if str(delete_id) in self.task:
+            del self.task[str(delete_id)]
 
